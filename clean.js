dbb4ccd0246e72bcd65335d60516232d73ce3131
Jeffrey C. Lu <ceeloo@gmail.com>
2020-04-04 11:57:13 -0400

    added files

---
+++ clean.js
@@ -0,0 +1,3 @@
+document.querySelector("#gateway-content").remove();
+document.querySelector(".css-1bd8bfl").remove();
+document.querySelector(".css-mcm29f").style.overflow = "auto";
---
+++ content.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    let paywallDiv = document.querySelector("#gateway-content");
+    paywallDiv.remove();
+    let gradientDiv = document.querySelector(".css-1bd8bfl");
+    gradientDiv.remove();
+    let articleDiv = document.querySelector(".css-mcm29f");
+    articleDiv.style.overflow = "auto";
+}
